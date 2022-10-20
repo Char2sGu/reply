c10c4b7b28717ec5de93542ff317a4753405e9a9
Char2s <Char2s@outlook.com>
2022-10-20 21:24:49 +0800

    style: lint

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+if (environment.production) enableProdMode();
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
