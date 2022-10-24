8b0f8a98a3e99852ef2f8193ebe20ba31420600a
Char2s <Char2s@outlook.com>
2022-10-24 17:44:51 +0800

    feat: implement nav logo arrow on mobile

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,11 +1,30 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { AnimationCurves } from '@angular/material/core';
 
 @Component({
   selector: 'rpl-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
+  animations: [
+    trigger('arrow', [
+      state('true', style({ transform: 'rotate(180deg)' })),
+      state('false', style({ transform: 'rotate(0deg)' })),
+      transition('true <=> false', [
+        animate(`200ms ${AnimationCurves.STANDARD_CURVE}`),
+      ]),
+    ]),
+  ],
 })
 export class NavComponent implements OnInit {
+  bottomMenuOpen = false;
+
   constructor() {}
 
   ngOnInit(): void {}
