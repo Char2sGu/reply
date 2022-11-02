2d912106dc098bcf3d2f6df2e331fd8e6199b2fd
Char2s <Char2s@outlook.com>
2022-11-02 13:07:58 +0800

    feat: implement mail card actions

--- src/app/shared/mail-card/mail-card.component.ts
+++ src/app/shared/mail-card/mail-card.component.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { BreakpointManager } from 'src/app/core/breakpoint.manager';
+
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @Component({
   selector: 'rpl-mail-card',
   standalone: true,
-  imports: [CommonModule, MatRippleModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatIconModule,
+    AvatarComponent,
+  ],
   templateUrl: './mail-card.component.html',
   styleUrls: ['./mail-card.component.scss'],
 })
 export class MailCardComponent implements OnInit {
-  constructor() {}
+  constructor(public breakpointManager: BreakpointManager) {}
 
   ngOnInit(): void {}
 }
