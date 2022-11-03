3ea8bee14fc7a829309a0edc7661b57747e8797e
Char2s <Char2s@outlook.com>
2022-11-04 01:00:41 +0800

    feat: create date distance pipe

---
+++ src/app/shared/date-distance.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateDistancePipe } from './date-distance.pipe';
+
+describe('DateDistancePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateDistancePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/date-distance.pipe.ts
@@ -0,0 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as timeago from 'timeago.js';
+
+const LOCALE_NAME = 'en_US_abbr';
+
+timeago.register(
+  LOCALE_NAME,
+  (_, index) =>
+    [
+      ['just now', 'just now'],
+      ['%s seconds ago', '%s secs ago'],
+      ['1 minute ago', '1 min ago'],
+      ['%s minutes ago', '%s mins ago'],
+      ['1 hour ago', '1 hr ago'],
+      ['%s hours ago', '%s hrs ago'],
+      ['1 day ago', '1 day ago'],
+      ['%s days ago', '%s days ago'],
+      ['1 week ago', '1 week ago'],
+      ['%s weeks ago', '%s weeks ago'],
+      ['1 month ago', '1 month ago'],
+      ['%s months ago', '%s months ago'],
+      ['1 year ago', '1 year ago'],
+      ['%s years ago', '%s years ago'],
+    ][index] as [string, string],
+);
+
+@Pipe({
+  name: 'dateDistance',
+  standalone: true,
+})
+export class DateDistancePipe implements PipeTransform {
+  transform(date: Date, ...args: unknown[]): string {
+    return timeago.format(date, LOCALE_NAME);
+  }
+}
