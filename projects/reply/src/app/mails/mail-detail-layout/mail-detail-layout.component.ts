05b4a3d1180577a865bccf735c37295c6c112b76
Char2s <Char2s@outlook.com>
2022-11-06 16:48:49 +0800

    feat(reply): implement mail detail page nav collapsing

--- projects/reply/src/app/mails/mail-detail-layout/mail-detail-layout.component.ts
+++ projects/reply/src/app/mails/mail-detail-layout/mail-detail-layout.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+import { Layout } from '@/app/core/layout.service';
+
 @Component({
   selector: 'rpl-mail-detail-layout',
   templateUrl: './mail-detail-layout.component.html',
@@ -7,7 +9,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MailDetailLayoutComponent implements OnInit {
-  constructor() {}
+  constructor(public layout: Layout) {}
 
   ngOnInit(): void {}
 }
