d67026e86ac8737bc69f45f4a24c6b9cf44118ee
Char2s <Char2s@outlook.com>
2022-11-09 17:42:24 +0800

    fix(reply): fix timeago.js locale

--- projects/reply/src/app/standalone/date-distance.pipe.ts
+++ projects/reply/src/app/standalone/date-distance.pipe.ts
@@ -7,24 +7,22 @@ timeago.register(
   LOCALE_NAME,
   (_, index) =>
     [
-      ['just now', 'just now'],
-      ['%s seconds ago', '%s secs ago'],
-      ['1 minute ago', '1 min ago'],
-      ['%s minutes ago', '%s mins ago'],
-      ['1 hour ago', '1 hr ago'],
-      ['%s hours ago', '%s hrs ago'],
-      ['1 day ago', '1 day ago'],
-      ['%s days ago', '%s days ago'],
-      ['1 week ago', '1 week ago'],
-      ['%s weeks ago', '%s weeks ago'],
-      ['1 month ago', '1 month ago'],
-      ['%s months ago', '%s months ago'],
-      ['1 year ago', '1 year ago'],
-      ['%s years ago', '%s years ago'],
+      ['just now', 'right now'],
+      ['%s secs ago', 'in %s secs'],
+      ['1 min ago', 'in 1 min'],
+      ['%s mins ago', 'in %s mins'],
+      ['1 hr ago', 'in 1 hr'],
+      ['%s hrs ago', 'in %s hrs'],
+      ['1 day ago', 'in 1 day'],
+      ['%s days ago', 'in %s days'],
+      ['1 week ago', 'in 1 week'],
+      ['%s weeks ago', 'in %s weeks'],
+      ['1 month ago', 'in 1 month'],
+      ['%s months ago', 'in %s months'],
+      ['1 year ago', 'in 1 year'],
+      ['%s years ago', 'in %s years'],
     ][index] as [string, string],
 );
-
-// TODO: fix the locale not working problem
 
 @Pipe({
   name: 'dateDistance',
