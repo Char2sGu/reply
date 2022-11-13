2cb134068976d5e82b67d2d6282a457a74c340ea
Char2s <Char2s@outlook.com>
2022-11-13 21:13:22 +0800

    refactor(reply): extract class reactive

---
+++ projects/reply/src/app/common/reactive.ts
@@ -0,0 +1,34 @@
+import { EventEmitter } from '@angular/core';
+import { debounceTime, map, Observable, shareReplay, startWith } from 'rxjs';
+
+export abstract class Reactive {
+  readonly value$: Observable<this>;
+
+  protected assign$: EventEmitter<void>;
+
+  constructor() {
+    this.assign$ = new EventEmitter();
+    this.value$ = this.assign$.pipe(
+      // Combine multiple assigns as well as trigger a new change detection
+      // cycle.
+      // Some components that is close to the root will not be able to be
+      // checked if we use the current running change detection cycle when
+      // the config is updated in an OnInit life-cycle hook.
+      debounceTime(0),
+
+      startWith(this),
+      map(() => this),
+      shareReplay(1),
+    );
+
+    return new Proxy(this, {
+      set: (target, prop, value) => {
+        const key = prop as keyof this;
+        if (target[key] === value) return true;
+        target[key] = value;
+        target.assign$.emit();
+        return true;
+      },
+    });
+  }
+}
--- projects/reply/src/app/core/layout.context.ts
+++ projects/reply/src/app/core/layout.context.ts
@@ -1,61 +1,28 @@
 import { ComponentPortal, Portal } from '@angular/cdk/portal';
-import { EventEmitter, Injectable } from '@angular/core';
-import {
-  debounceTime,
-  filter,
-  map,
-  Observable,
-  shareReplay,
-  startWith,
-} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { filter } from 'rxjs';
 
+import { Reactive } from '../common/reactive';
 import { SearchButtonComponent } from '../standalone/search-button/search-button.component';
 import { RouterStatus } from './router-status.state';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LayoutContext {
+export class LayoutContext extends Reactive {
   contentFavored;
   navFabConfig;
   navBottomActionsPortal: Portal<unknown>;
 
-  readonly value$: Observable<this>;
-  private assign$ = new EventEmitter();
-
   constructor(routerStatus: RouterStatus) {
+    super();
+
     this.contentFavored = false;
     this.navFabConfig = { text: 'Compose', icon: 'edit', link: '/compose' };
     this.navBottomActionsPortal = new ComponentPortal(SearchButtonComponent);
 
-    this.value$ = this.assign$.pipe(
-      // Combine multiple assigns as well as trigger a new change detection
-      // cycle.
-      // Some components that is close to the root will not be able to be
-      // checked if we use the current running change detection cycle when
-      // the config is updated in an OnInit life-cycle hook.
-      debounceTime(0),
-
-      startWith(this),
-      map(() => this),
-      shareReplay(1),
-    );
-
     routerStatus.navigating$
       .pipe(filter((navigating) => navigating))
       .subscribe(() => (this.contentFavored = false));
-
-    return new Proxy(this, {
-      set: (target, prop, value) => {
-        const exists = prop in target;
-        if (!exists) return false;
-
-        const key = prop as keyof this;
-        if (target[key] === value) return true;
-        target[key] = value;
-        target.assign$.emit();
-        return true;
-      },
-    });
   }
 }
