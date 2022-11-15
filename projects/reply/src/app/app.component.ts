6ffd93ab4fbed5a5243e9555f888618ac76a92d5
Char2s <Char2s@outlook.com>
2022-11-15 12:31:42 +0800

    feat(reply): implement transition between foundation and search

--- projects/reply/src/app/app.component.ts
+++ projects/reply/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ChildrenOutletContexts } from '@angular/router';
 
+import { SharedAxisAnimation } from './core/animations';
 import { BreakpointManager, BreakpointMap } from './core/breakpoint.manager';
 
 @Component({
@@ -11,6 +13,21 @@ import { BreakpointManager, BreakpointMap } from './core/breakpoint.manager';
   animations: [
     // remove the initial entering animations of nav elements
     trigger('nav', [transition(':enter', [])]),
+    trigger('wrapper', [
+      transition(':enter, :leave, * <=> Empty', []),
+      transition('FoundationComponent => *', [
+        SharedAxisAnimation.apply('z', 'forward', {
+          incoming: ':enter',
+          outgoing: ':leave rpl-content, :leave rpl-nav',
+        }),
+      ]),
+      transition('* => FoundationComponent', [
+        SharedAxisAnimation.apply('z', 'backward', {
+          incoming: ':enter rpl-content, :enter rpl-nav',
+          outgoing: ':leave',
+        }),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
@@ -23,9 +40,19 @@ export class AppComponent {
 
   breakpoints$ = this.breakpointManager.breakpoints$;
 
-  constructor(private breakpointManager: BreakpointManager) {
+  constructor(
+    private breakpointManager: BreakpointManager,
+    private childRouterOutletContexts: ChildrenOutletContexts,
+  ) {
     this.breakpointManager.breakpoints$.subscribe((v) => {
       this.breakpointMap = v;
     });
+  }
+
+  getRouteComponentName(): string {
+    return (
+      this.childRouterOutletContexts.getContext('primary')?.route?.component
+        ?.name ?? 'Empty'
+    );
   }
 }
