ae9f9d6f027d83e0ed03d566509fef4f71b342ab
Char2s <Char2s@outlook.com>
2022-11-19 17:54:06 +0800

    feat(reply): implement true layout animation

--- projects/reply/src/app/layout-projection/layout-animation.directive.ts
+++ projects/reply/src/app/layout-projection/layout-animation.directive.ts
@@ -1,5 +1,14 @@
-import { animate, AnimationBuilder, style } from '@angular/animations';
 import { Directive, Input, OnInit, Self } from '@angular/core';
+import {
+  animate,
+  cubicBezier,
+  easeIn,
+  easeInOut,
+  easeOut,
+  Easing,
+  linear,
+  mix,
+} from 'popmotion';
 import {
   animationFrames,
   BehaviorSubject,
@@ -35,17 +44,12 @@ export class LayoutAnimationDirective implements OnInit {
   }
   private animateLayoutOn$ = new BehaviorSubject<Observable<void>>(EMPTY);
 
-  @Input() animationTimings: string | number = '225ms ease-in-out';
+  @Input() animationDuration: number = 225;
+  @Input() animationEasing: string = 'ease-in-out';
 
-  private layoutSnapshots = new WeakMap<
-    LayoutProjectionNode,
-    LayoutProjectionLayout
-  >();
+  private layoutSnapshots = new NodeLayoutWeakMap();
 
-  constructor(
-    @Self() private node: LayoutProjectionNode,
-    private animationBuilder: AnimationBuilder,
-  ) {}
+  constructor(@Self() private node: LayoutProjectionNode) {}
 
   ngOnInit(): void {
     const domWillUpdate$ = this.animateLayoutOn$.pipe(exhaustAll(), skip(1));
@@ -55,10 +59,7 @@ export class LayoutAnimationDirective implements OnInit {
         tap(() => this.snapshot()),
         switchMap(() => animationFrames().pipe(first())),
       )
-      .subscribe(() => {
-        this.project();
-        this.animate();
-      });
+      .subscribe(() => this.animate());
   }
 
   snapshot(): void {
@@ -67,28 +68,87 @@ export class LayoutAnimationDirective implements OnInit {
     });
   }
 
-  project(): void {
+  animate(): void {
+    this.node.measure();
+
+    const sourceLayoutMap = new NodeLayoutWeakMap();
+    const destLayoutMap = new NodeLayoutWeakMap();
+
+    this.node.traverse((node) => {
+      const sourceLayout = node.layout;
+      if (!sourceLayout) throw new Error('Unknown node');
+      sourceLayoutMap.set(node, sourceLayout);
+
+      const snapshot = this.layoutSnapshots.get(node);
+      this.layoutSnapshots.delete(node);
+      const destLayout = snapshot ?? node.element.getBoundingClientRect();
+      destLayoutMap.set(node, destLayout);
+    });
+
+    const project = (progress: number) =>
+      this.project(sourceLayoutMap, destLayoutMap, progress);
+
+    project(0);
+    animate({
+      from: 0,
+      to: 1,
+      duration: this.animationDuration,
+      ease: resolveEasingString(this.animationEasing),
+      onUpdate: project,
+    });
+  }
+
+  project(
+    sourceLayoutMap: NodeLayoutWeakMap,
+    destLayoutMap: NodeLayoutWeakMap,
+    progress: number,
+  ): void {
     this.node.measure();
 
     this.node.traverse((node) => {
-      const snapshot = this.layoutSnapshots.get(node);
-      if (!snapshot) return;
-      node.calculate(snapshot);
+      const sourceLayout = sourceLayoutMap.get(node);
+      const destLayout = destLayoutMap.get(node);
+      if (!sourceLayout || !destLayout) throw new Error('Unknown node');
+
+      const frameTargetLayout: LayoutProjectionLayout = {
+        top: mix(destLayout.top, sourceLayout.top, progress),
+        left: mix(destLayout.left, sourceLayout.left, progress),
+        right: mix(destLayout.right, sourceLayout.right, progress),
+        bottom: mix(destLayout.bottom, sourceLayout.bottom, progress),
+        width: mix(destLayout.width, sourceLayout.width, progress),
+        height: mix(destLayout.height, sourceLayout.height, progress),
+      };
+
+      node.calculate(frameTargetLayout);
     });
 
     this.node.project();
   }
+}
 
-  animate(): void {
-    this.node.traverse((node) => {
-      const animationPlayer = this.animationBuilder
-        .build([animate(this.animationTimings, style({ transform: 'none' }))])
-        .create(node.element);
-      animationPlayer.play();
-      animationPlayer.onDone(() => {
-        node.element.style.transform = '';
-        animationPlayer.destroy();
-      });
-    });
+class NodeLayoutWeakMap extends WeakMap<
+  LayoutProjectionNode,
+  LayoutProjectionLayout
+> {}
+
+function resolveEasingString(easing: string): Easing {
+  if (easing === 'linear') {
+    return linear;
+  } else if (easing === 'ease') {
+    return easeInOut;
+  } else if (easing === 'ease-in') {
+    return easeIn;
+  } else if (easing === 'ease-out') {
+    return easeOut;
+  } else if (easing === 'ease-in-out') {
+    return easeInOut;
+  } else if (easing.startsWith('cubic-bezier')) {
+    const [a, b, c, d] = easing
+      .replace('cubic-bezier(', '')
+      .replace(')', '')
+      .split(',')
+      .map((v) => parseFloat(v));
+    return cubicBezier(a, b, c, d);
   }
+  throw new Error(`Invalid easing string: ${easing}`);
 }
