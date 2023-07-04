fa676868298a2837e29066d0f439f3e054338969
Char2s <Char2s@outlook.com>
2023-07-05 01:45:09 +0800

    refactor(reply): reduce app initializers

--- projects/reply/src/app/core/layout-context.token.ts
+++ projects/reply/src/app/core/layout-context.token.ts
@@ -1,21 +1,29 @@
 import {
+  inject,
   InjectionToken,
   signal,
   TemplateRef,
   WritableSignal,
 } from '@angular/core';
-import { Params } from '@angular/router';
+import { NavigationStart, Params, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 export const LAYOUT_CONTEXT = new InjectionToken<WritableSignal<LayoutContext>>(
   'LAYOUT_CONTEXT',
   {
     providedIn: 'root',
-    factory: () =>
-      signal({
+    factory: () => {
+      const router = inject(Router);
+      const context = signal({
         contentFavored: false,
         navFabConfig: null,
         navBottomActions: null,
-      }),
+      });
+      router.events
+        .pipe(filter((event) => event instanceof NavigationStart))
+        .subscribe(() => context.mutate((c) => (c.contentFavored = false)));
+      return context;
+    },
   },
 );
 
