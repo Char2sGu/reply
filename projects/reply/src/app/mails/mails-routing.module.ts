2918dfdf4ba46976109dbc8323394a95cf1f9f71
Char2s <Char2s@outlook.com>
2023-07-04 12:56:14 +0800

    build: upgrade to angular v16

--- projects/reply/src/app/mails/mails-routing.module.ts
+++ projects/reply/src/app/mails/mails-routing.module.ts
@@ -1,29 +1,15 @@
-import { Injectable, NgModule } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  Resolve,
-  RouterModule,
-  Routes,
-} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MailDetailLayoutComponent } from './mail-detail-layout/mail-detail-layout.component';
 import { MailListLayoutComponent } from './mail-list-layout/mail-list-layout.component';
 import { MailsComponent } from './mails.component';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class MailboxRouteTitleResolver implements Resolve<string> {
-  resolve(route: ActivatedRouteSnapshot): string {
-    return route.params['mailboxName'];
-  }
-}
-
 const routes: Routes = [
   {
     path: '',
     component: MailsComponent,
-    title: MailboxRouteTitleResolver,
+    title: (route) => route.params['mailboxName'],
     children: [
       {
         path: '',
