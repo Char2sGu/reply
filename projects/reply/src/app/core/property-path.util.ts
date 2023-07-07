fb7edfd5f01f290c0ff44bb68ed4cfd257abb991
Char2s <Char2s@outlook.com>
2023-07-08 05:19:16 +0800

    fix(reply): fix PropertyPathNonNullable

--- projects/reply/src/app/core/property-path.util.ts
+++ projects/reply/src/app/core/property-path.util.ts
@@ -23,31 +23,48 @@ export type PropertyPathNonNullable<
   T extends object,
   P extends PropertyPath<T>,
 > = {
-  [K in Exclude<keyof T, P>]: T[K];
+  [K in string & keyof T as (K | `${K}.${string}`) & P extends never
+    ? K
+    : never]: T[K];
 } & {
-  [K in P as `${K}.${string}` & P extends never
-    ? K extends `${infer A}.${string}`
-      ? A
-      : K
-    : never]-?: K extends string & keyof T
-    ? NonNullable<T[K]>
-    : K extends `${infer A extends string & keyof T}.${infer B}`
-    ? NonNullable<T[A]> extends infer O extends object
-      ? B extends PropertyPath<O>
-        ? PropertyPathNonNullable<O, B>
+  [K in string & keyof T as (K | `${K}.${string}`) & P extends never
+    ? never
+    : K]-?: NonNullable<T[K]> extends infer V
+    ? `${K}.${string}` & P extends never
+      ? V
+      : V extends object
+      ? `${K}.${string}` & P extends `${K}.${infer PP extends PropertyPath<V>}`
+        ? PropertyPathNonNullable<V, PP>
         : never
       : never
     : never;
 };
 
-// type Test = PropertyPathNonNullable<
-//   {
-//     a: { aa?: 'aa' };
-//     b: { bb?: 'bb' };
-//     c: 'c';
-//     d: 'd';
-//     e?: { ee: 'ee' };
-//     f: { ff: 'ff'; fff: 'fff' };
-//   },
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// interface TestObj {
+//   a: { aa?: 'aa' };
+//   b: { bb?: 'bb' };
+//   c: 'c';
+//   d: 'd';
+//   e?: { ee: 'ee' };
+//   f: { ff: 'ff'; fff: 'fff' };
+// }
+
+// type TestResult = PropertyPathNonNullable<
+//   TestObj,
 //   'a.aa' | 'b' | 'b.bb' | 'c' | 'e.ee' | 'f.ff' | 'f.fff'
 // >;
+
+// declare const test1: PropertyPathNonNullable<TestObj, 'a.aa'>;
+// const assertion1: 'aa' = test1.a.aa;
+// declare const test2: PropertyPathNonNullable<TestObj, 'b' | 'b.bb'>;
+// const assertion2: 'bb' = test2.b.bb;
+// declare const test3: PropertyPathNonNullable<TestObj, 'c'>;
+// const assertion3: 'c' = test3.c;
+// declare const test4: PropertyPathNonNullable<TestObj, 'c'>;
+// const assertion4: 'd' = test4.d;
+// declare const test5: PropertyPathNonNullable<TestObj, 'e.ee'>;
+// const assertion5: 'ee' = test5.e.ee;
+// declare const test6: PropertyPathNonNullable<TestObj, 'f.ff' | 'f.fff'>;
+// const assertion6: { ff: 'ff'; fff: 'fff' } = test6.f;
+/* eslint-enable @typescript-eslint/no-unused-vars */
