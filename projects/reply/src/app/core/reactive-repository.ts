b042de24ce8a1ac616a34ce20ef6b594d095e385
Char2s <Char2s@outlook.com>
2023-07-10 16:43:57 +0800

    docs(reply): document reactive-repository.ts

--- projects/reply/src/app/core/reactive-repository.ts
+++ projects/reply/src/app/core/reactive-repository.ts
@@ -15,14 +15,26 @@ import {
   EntityNotFoundException,
 } from './exceptions';
 
+/**
+ * A reactive repository serves as the one and only source of truth for any
+ * entities of a given type.
+ */
 export abstract class ReactiveRepository<Entity> {
   protected updatesSubject = new Subject<ReactiveRepositoryUpdate<Entity>>();
   readonly updates$ = this.updatesSubject.asObservable();
 
   protected entities = new Map<string, BehaviorSubject<Entity>>();
 
+  /**
+   * @returns A unique identifier for the given entity, usually `entity.id`.
+   */
   abstract identify(entity: Entity): string;
 
+  /**
+   * @returns An observable of all entities matching the given condition. A new
+   * value is emitted whenever an {@link insert}, {@link patch}, or
+   * {@link delete} operation is performed on an entity matching the condition.
+   */
   query(
     condition: (entity: Entity) => boolean = () => true,
   ): Observable<Entity[]> {
@@ -47,12 +59,23 @@ export abstract class ReactiveRepository<Entity> {
     );
   }
 
+  /**
+   * @throws An {@link EntityNotFoundException} if entity with the given id
+   * doesn't exist.
+   * @returns An observable of the entity with the given id. A new value is
+   * emitted when the entity is updated; the observable completes when the
+   * entity is deleted.
+   */
   retrieve(id: string): Observable<Entity> {
     const entity = this.entities.get(id);
     if (!entity) throw new EntityNotFoundException();
     return entity;
   }
 
+  /**
+   * @throws An {@link EntityDuplicateException} if entity with the same id
+   * already exists.
+   */
   insert(entity: Entity): ReactiveRepositoryUpdate<Entity> {
     const id = this.identify(entity);
     if (this.entities.has(id)) throw new EntityDuplicateException();
@@ -67,6 +90,10 @@ export abstract class ReactiveRepository<Entity> {
     });
   }
 
+  /**
+   * @throws An {@link EntityNotFoundException} if entity with the given id
+   * doesn't exist.
+   */
   patch(
     id: string,
     payload: Partial<Entity>,
@@ -84,6 +111,10 @@ export abstract class ReactiveRepository<Entity> {
     });
   }
 
+  /**
+   * {@link insert Insert} the entity if it doesn't exist, otherwise
+   * {@link patch} the existing one.
+   */
   insertOrPatch(entity: Entity): ReactiveRepositoryUpdate<Entity> {
     const id = this.identify(entity);
     const existing = this.entities.get(id);
@@ -91,6 +122,10 @@ export abstract class ReactiveRepository<Entity> {
     return this.insert(entity);
   }
 
+  /**
+   * @throws An {@link EntityNotFoundException} if entity with the given id
+   * doesn't exist.
+   */
   delete(id: string): ReactiveRepositoryUpdate<Entity> {
     const entity$ = this.entities.get(id);
     if (!entity$) throw new EntityNotFoundException();
@@ -106,6 +141,10 @@ export abstract class ReactiveRepository<Entity> {
     });
   }
 
+  /**
+   * @returns An observable of whether an entity with the given id exists. A new
+   * value is emitted when the result is updated.
+   */
   exists(id: string): Observable<boolean> {
     let result = this.entities.has(id);
     return this.updates$.pipe(
@@ -128,11 +167,26 @@ export abstract class ReactiveRepository<Entity> {
   }
 }
 
+/**
+ * An update to an entity in a {@link ReactiveRepository}. This object is
+ * observable-interoperable: subscribing to it is equivalent to subscribing to
+ * the {@link entity$} property.
+ */
 export interface ReactiveRepositoryUpdate<Entity>
   extends InteropObservable<Entity> {
   readonly id: string;
   readonly prev: Entity | null;
   readonly curr: Entity | null;
+
+  /**
+   * An observable of the target entity. A new value is emitted when the entity
+   * is updated; the observable completes when the entity is deleted.
+   */
   readonly entity$: Observable<Entity>;
+
+  /**
+   * Perform another operation aiming to undo the operation that caused this
+   * update.
+   */
   undo(): ReactiveRepositoryUpdate<Entity>;
 }
