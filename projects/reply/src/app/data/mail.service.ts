9afb86ef7de94ce59f5fb587e7418e625bee89f5
Char2s <Char2s@outlook.com>
2023-07-10 14:23:27 +0800

    feat(reply): add more methods to mail service

--- projects/reply/src/app/data/mail.service.ts
+++ projects/reply/src/app/data/mail.service.ts
@@ -2,9 +2,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Mail } from './mail.model';
+import { Mailbox } from './mailbox.model';
 
 @Injectable()
 export abstract class MailService {
   abstract loadMails(): Observable<Mail[]>;
-  abstract loadMail(id: string): Observable<Mail>;
+  abstract loadMail(id: Mail['id']): Observable<Mail>;
+  abstract markMailAsStarred(id: Mail['id']): Observable<void>;
+  abstract markMailAsNotStarred(id: Mail['id']): Observable<void>;
+  abstract markMailAsRead(id: Mail['id']): Observable<void>;
+  abstract markMailAsUnread(id: Mail['id']): Observable<void>;
+  abstract moveMail(id: Mail['id'], mailboxId: Mailbox['id']): Observable<void>;
 }
--- projects/reply/src/app/google-backend/google-mail.service.ts
+++ projects/reply/src/app/google-backend/google-mail.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import { combineLatest, map, Observable, switchMap } from 'rxjs';
+import { combineLatest, first, map, Observable, switchMap } from 'rxjs';
 
 import { access } from '../core/property-path.utils';
 import { Mail } from '../data/mail.model';
 import { MailRepository } from '../data/mail.repository';
 import { MailService } from '../data/mail.service';
+import { MailboxRepository } from '../data/mailbox.repository';
 import { GmailMessageParser } from './core/gmail-message-parser.service';
 import { useGoogleApi } from './core/google-apis.utils';
 
@@ -12,9 +13,11 @@ import { useGoogleApi } from './core/google-apis.utils';
 export class GoogleMailService implements MailService {
   private messageParser = inject(GmailMessageParser);
   private mailRepo = inject(MailRepository);
+  private mailboxRepo = inject(MailboxRepository);
 
   private messageListApi = useGoogleApi((a) => a.gmail.users.messages.list);
   private messageGetApi = useGoogleApi((a) => a.gmail.users.messages.get);
+  private messageModifyApi = useGoogleApi((a) => a.gmail.users.messages.modify);
 
   loadMails(): Observable<Mail[]> {
     return this.messageListApi({ userId: 'me' }).pipe(
@@ -25,11 +28,47 @@ export class GoogleMailService implements MailService {
     );
   }
 
-  loadMail(id: string): Observable<Mail> {
+  loadMail(id: Mail['id']): Observable<Mail> {
     return this.messageGetApi({ userId: 'me', id }).pipe(
       map((response) => response.result),
       switchMap((message) => this.messageParser.parseFullMessage(message)),
       switchMap((mail) => this.mailRepo.insertOrPatch(mail)),
+    );
+  }
+
+  markMailAsStarred(id: Mail['id']): Observable<void> {
+    return this.modifyMessage(id, { addLabelIds: ['STARRED'] });
+  }
+  markMailAsNotStarred(id: Mail['id']): Observable<void> {
+    return this.modifyMessage(id, { removeLabelIds: ['STARRED'] });
+  }
+
+  markMailAsRead(id: string): Observable<void> {
+    return this.modifyMessage(id, { removeLabelIds: ['UNREAD'] });
+  }
+  markMailAsUnread(id: string): Observable<void> {
+    return this.modifyMessage(id, { addLabelIds: ['UNREAD'] });
+  }
+
+  moveMail(id: string, mailboxId: string): Observable<void> {
+    return this.mailboxRepo
+      .retrieve(mailboxId)
+      .pipe(
+        switchMap((mailbox) =>
+          this.modifyMessage(id, { addLabelIds: [mailbox.id] }),
+        ),
+      );
+  }
+
+  private modifyMessage(
+    id: Mail['id'],
+    body: gapi.client.gmail.ModifyMessageRequest,
+  ): Observable<void> {
+    return this.messageModifyApi({ userId: 'me', id }, body).pipe(
+      switchMap((response) => this.messageParser.parseMessage(response.result)),
+      switchMap((mail) => this.mailRepo.patch(id, mail)),
+      first(),
+      map(() => undefined),
     );
   }
 }
