ad814b85672c9b6508a7bb21042dcc47f2451c00
Char2s <Char2s@outlook.com>
2023-07-14 14:03:38 +0800

    feat(reply): outline mail actions menu

---
+++ projects/reply/src/app/mails/core/mail-action-menu-button/mail-action-menu-button.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'rpl-mail-action-menu-button',
+  templateUrl: './mail-action-menu-button.component.html',
+  styleUrls: ['./mail-action-menu-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MailActionMenuButtonComponent {}
--- projects/reply/src/app/mails/mails.module.ts
+++ projects/reply/src/app/mails/mails.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
 import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 import { LayoutProjectionModule } from '@layout-projection/angular';
 import { ScrollingModule } from '@reply/scrolling';
@@ -17,6 +18,7 @@ import { ReattachOnChangeDirective } from '../shared/reattach-on-change.directiv
 import { ScrollableAreaComponent } from '../shared/scrollable-area/scrollable-area.component';
 import { SearchButtonComponent } from '../shared/search-button/search-button.component';
 import { MailComponent } from './core/mail/mail.component';
+import { MailActionMenuButtonComponent } from './core/mail-action-menu-button/mail-action-menu-button.component';
 import { MailCardComponent } from './core/mail-card/mail-card.component';
 import { MailCardAnimationPresenceComponent } from './core/mail-card-animation-presence/mail-card-animation-presence.component';
 import { MailCardListComponent } from './core/mail-card-list/mail-card-list.component';
@@ -26,6 +28,8 @@ import { MailDetailLayoutComponent } from './mail-detail-layout/mail-detail-layo
 import { MailListLayoutComponent } from './mail-list-layout/mail-list-layout.component';
 import { MailsComponent } from './mails.component';
 import { MailsRoutingModule } from './mails-routing.module';
+
+// TODO: move actions to actions menu when screen is becoming smaller
 
 @NgModule({
   declarations: [
@@ -38,12 +42,14 @@ import { MailsRoutingModule } from './mails-routing.module';
     MailStarButtonComponent,
     MailDeleteButtonComponent,
     MailCardAnimationPresenceComponent,
+    MailActionMenuButtonComponent,
   ],
   imports: [
     CommonModule,
     MailsRoutingModule,
     MatButtonModule,
     MatTooltipModule,
+    MatMenuModule,
     MatRippleModule,
     MatIconModule,
     LayoutProjectionModule,
