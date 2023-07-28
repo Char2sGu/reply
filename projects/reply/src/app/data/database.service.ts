6b5eb72fe88f58db491bcef94b2cb86bef8b9dbf
Char2s <Char2s@outlook.com>
2023-07-28 20:49:55 +0800

    feat(reply): implement indexed-db infrastructure

---
+++ projects/reply/src/app/data/database.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import Dexie, { Table } from 'dexie';
+
+import { Contact } from '@/app/data/contact.model';
+import { Mail } from '@/app/data/mail.model';
+import { Mailbox } from '@/app/data/mailbox.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Database extends Dexie {
+  mails!: Table<Mail, Mail['id']>;
+  mailboxes!: Table<Mailbox, Mailbox['id']>;
+  contacts!: Table<Contact, Contact['id']>;
+
+  constructor() {
+    super('reply');
+    this.version(1).stores({
+      mails: 'id',
+      mailboxes: 'id',
+      contacts: 'id',
+    });
+  }
+}
