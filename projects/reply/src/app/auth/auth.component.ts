7cba95b9e64810da0f056950566840723397e39c
Char2s <Char2s@outlook.com>
2023-08-06 12:38:27 +0800

    fix(reply): fix stream error

--- projects/reply/src/app/auth/auth.component.ts
+++ projects/reply/src/app/auth/auth.component.ts
@@ -4,7 +4,7 @@ import {
   EventEmitter,
   inject,
 } from '@angular/core';
-import { filter, first, map, Observable, switchMap } from 'rxjs';
+import { filter, first, map, switchMap } from 'rxjs';
 
 import { AuthenticationConductor } from '../core/auth/authentication.conductor';
 import { AuthenticationService } from '../core/auth/authentication.service';
@@ -27,29 +27,27 @@ export class AuthComponent {
   checked = false;
   loading$ = this.authService.authorized$;
 
-  private lastAccount$ = this.queryLastAccount();
-
   constructor() {
     this.buttonClick
       .pipe(switchMap(() => this.authConductor.requestAuthorization()))
       .subscribe();
-    this.lastAccount$
+
+    this.accountRepo
+      .query()
       .pipe(
-        filter(Boolean),
+        map((accounts) => this.findLatestAuthorizedAccount(accounts)),
         first(),
+        filter(Boolean),
         switchMap((a) => this.authConductor.requestAuthorization(a.id)),
       )
       .subscribe();
   }
 
-  private queryLastAccount(): Observable<Account | undefined> {
-    return this.accountRepo.query().pipe(
-      first(),
-      map((accounts) =>
-        accounts
-          .sort((a, b) => a.authorizedAt.getTime() - b.authorizedAt.getTime())
-          .at(-1),
-      ),
+  private findLatestAuthorizedAccount(accounts: Account[]): Account | null {
+    return (
+      accounts
+        .sort((a, b) => a.authorizedAt.getTime() - b.authorizedAt.getTime())
+        .at(-1) ?? null
     );
   }
 }
